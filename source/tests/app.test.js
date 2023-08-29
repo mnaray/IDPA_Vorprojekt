bfbf9f06a881a5374b19a8a69c03ee2024bf3f88
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-08-29 15:16:45 +0200

    add example test

--- source/tests/app.test.js
+++ source/tests/app.test.js
@@ -1,9 +1,11 @@
-/**
- * @jest-environment jsdom
- */
 import { HelloWorld } from "../app";
 
 test("should greet the world", () => {
     expect(typeof HelloWorld()).toBe("string");
     expect(HelloWorld().includes("Hello")).toBe(true);
+});
+
+test("should fail (example)", () => {
+    expect(typeof HelloWorld()).toBe("string");
+    expect(HelloWorld().includes("Hello")).toBe(false);
 });
