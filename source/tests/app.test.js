abff902da8b1e75a35e3ada77e9ef93f42580c3f
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-08-29 14:48:09 +0200

    create test functions for setup

---
+++ source/tests/app.test.js
@@ -0,0 +1,6 @@
+import { HelloWorld } from "../app";
+
+test("should greet the world", () => {
+    expect(typeof HelloWorld()).toBe("string");
+    expect(HelloWorld().includes("Hello")).toBe(true);
+});
