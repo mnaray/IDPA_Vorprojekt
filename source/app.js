a493c1bc597e590c3af61d6bce9a0a4a078aaa30
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-09-05 10:32:27 +0200

    add readValues function
    Co-authored-by: CevapiMitKajmak Crunch2404@hotmail.com

--- source/app.js
+++ source/app.js
@@ -7,7 +7,25 @@ export function onSubmit(event) {
 }
 
 export function readValues() {
-    throw new Error();
+    try {
+        const balance = parseInputToFloat("balance");
+        const interest = parseInputToFloat("interest");
+        const date = document.getElementById("birthday").value;
+
+        return { balance, interest, date };
+    } catch (err) {
+        console.error(err.message);
+        return;
+    }
+
+    function parseInputToFloat(inputId) {
+        const inputValue = document.getElementById(inputId).value;
+        if (isNaN(inputValue)) {
+            throw new Error("Bitte nur Zahlen eingeben!");
+        }
+        const floatValue = parseFloat(inputValue);
+        return floatValue;
+    }
 }
 
 export function validate(balance, interest, birthday) {
