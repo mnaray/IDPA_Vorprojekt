b748d12705baf342bc74a673040885726d21ca8e
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-09-05 08:36:40 +0200

    write unittests for validation

--- source/tests/app.test.js
+++ source/tests/app.test.js
@@ -1,11 +1,42 @@
-import { HelloWorld } from "../app";
+import { HelloWorld, validate } from "../app";
 
 test("should greet the world", () => {
     expect(typeof HelloWorld()).toBe("string");
     expect(HelloWorld().includes("Hello")).toBe(true);
 });
 
-test("should fail (example)", () => {
-    expect(typeof HelloWorld()).toBe("string");
-    expect(HelloWorld().includes("Hello")).toBe(false);
+test("should be true for valid values", () => {
+    const actual = validate(2032.5, 2.21, "1999-12-12");
+    expect(typeof actual).toBe("bool");
+    expect(actual).toBe(true);
+});
+
+test("should be false for invalid interest type", () => {
+    const actual = validate(2032, "a", "1999-12-12");
+    expect(typeof actual).toBe("bool");
+    expect(actual).toBe(false);
+});
+
+test("should be false for invalid balance type", () => {
+    const actual = validate("a", 2.21, "1999-12-12");
+    expect(typeof actual).toBe("bool");
+    expect(actual).toBe(false);
+});
+
+test("should be false for invalid interest value", () => {
+    const actual = validate(2032, -2.21, "1999-12-12");
+    expect(typeof actual).toBe("bool");
+    expect(actual).toBe(false);
+});
+
+test("should be false for invalid balance value", () => {
+    const actual = validate(-2032, 2.21, "1999-12-12");
+    expect(typeof actual).toBe("bool");
+    expect(actual).toBe(false);
+});
+
+test("should be false for future date", () => {
+    const actual = validate(2032, 2.21, "2050-12-12");
+    expect(typeof actual).toBe("bool");
+    expect(actual).toBe(false);
 });
